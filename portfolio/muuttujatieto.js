4142352baca85cf4d6a6ca920d49a8fa666f47a3
Flafiee <whitenremi@gmail.com>
2025-05-09 09:25:41 +0300

    js

--- portfolio/muuttujatieto.js
+++ portfolio/muuttujatieto.js
@@ -1,4 +1,17 @@
+console.log("Muuttujatieto avattu")
 a=3
 b=5
 c=7
 d=15
+function summa(a, b) {
+    return a + b;
+}
+function vähennys(b, c) {
+    return b - c;
+}
+function kerto(c, d) {
+    return c * d;
+}
+function jako(d, a) {
+    return d / a;
+}
