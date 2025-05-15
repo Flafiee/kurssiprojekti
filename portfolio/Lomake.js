f39e2e69ef94d1f25c1eaf9558f4a69e3a3b3459
Flafiee <whitenremi@gmail.com>
2025-05-15 09:57:13 +0300

    keskustelu js

--- portfolio/Lomake.js
+++ portfolio/Lomake.js
@@ -1,1 +1,32 @@
 console.log("Lomakesivu avattu");
+   
+const openDialogBtn = document.getElementById('open-dialog-btn');
+const closeDialogBtn = document.getElementById('close-dialog-btn');
+const commentDialog = document.getElementById('comment-dialog');
+const submitBtn = document.getElementById('submit-btn');
+
+openDialogBtn.addEventListener('click', () => {
+    commentDialog.showModal();
+});
+
+
+closeDialogBtn.addEventListener('click', () => {
+    commentDialog.close();
+});
+
+
+submitBtn.addEventListener('click', () => {
+    const fname = document.getElementById('fname').value;
+    const lname = document.getElementById('lname').value;
+    const email = document.getElementById('email').value;
+    const birthday = document.getElementById('birthday').value;
+    const message = document.getElementById('message').value;
+
+    console.log("Etunimi:", fname);
+    console.log("Sukunimi:", lname);
+    console.log("Sähköposti:", email);
+    console.log("Syntymäpäivä:", birthday);
+    console.log("Viesti:", message);
+
+    commentDialog.close();
+});
