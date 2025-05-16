79d641278115c0e2e9f0f5e42d01b056b06b0742
Flafiee <whitenremi@gmail.com>
2025-05-16 10:44:30 +0300

    teht lista + js +

--- portfolio/Tehtävälista.js
+++ portfolio/Tehtävälista.js
@@ -15,10 +15,14 @@ function paivitaLista() {
 function lisaa(){ 
     onclick="lisaa()"
     const uusiTehtava = document.getElementById("tehtavaInput").value;
-    if (uusiTehtava) {
-        lista.push(uusiTehtava);
+    const maara = parseInt(document.getElementById("tehtavaInput2").value, 10);
+    if (uusiTehtava.trim() !== "" && !isNaN(maara) && maara > 0) {
+        for (let i = 0; i < maara; i++) {
+            lista.push(uusiTehtava);
+        }
         paivitaLista();
         document.getElementById("tehtavaInput").value = "";
+        document.getElementById("tehtavaInput2").value = "";
     }
-
 }
+document.addEventListener("DOMContentLoaded",paivitaLista)
